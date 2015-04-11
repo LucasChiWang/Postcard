838fd0e41e4745b08b36b283ffe259daae34cbdd
Lucas <lucas.j.house@gmail.com>
2015-04-12 01:04:14 +1000

    nameLabel Added

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,11 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterMessageTextField: UITextField!
     
     @IBOutlet weak var mailButton: UIButton!
-    
-    
-    
-    
-    
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,17 +26,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func sendMailButton(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-        
-        
         enterMessageTextField.text = ""
-    
         enterMessageTextField.resignFirstResponder()
-    
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
     }
 
 }
